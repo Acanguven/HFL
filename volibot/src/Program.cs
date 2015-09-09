6dd68d56548d44e14c98abbba749735e20f788c2
The Law <ahmetcanguven44@gmail.com>
2015-09-09 07:34:07 +0300

    2.4

--- volibot/src/Program.cs
+++ volibot/src/Program.cs
@@ -33,7 +33,7 @@ namespace HandsFreeLeveler
         public static int maxLevel = 31;
         public static string qType = "INTRO_BOT";
         public static bool started = false;
-        public static float version = 2.3f;
+        public static float version = 2.4f;
         public static bool buyBoost = false;
         public static bool rndSpell = false;
         public static string spell1 = "GHOST";
